6108182937f74f3e9673b22e6f6c58df43b3bf58
bitoller <bi_toller@hotmail.com>
2023-11-22 21:35:10 -0300

    small changes

--- src/scripts/treatment.js
+++ src/scripts/treatment.js
@@ -1,10 +1,28 @@
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "../assets/img/menuIcon.svg";
+  let menuMobile = document.querySelector(".mobile-menu");
+
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "../assets/img/menuIcon.svg";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "../assets/img/menuIcon.svg";
+  }
+}
+
+function displayRadioResult() {
+  let selectedOption = document.querySelector(
+    "input[name='mortality']:checked"
+  );
+
+  if (selectedOption) {
+    let textResult = "";
+
+    if (selectedOption.value === "yes") {
+      textResult = "Você escolheu SIM. Isso é ótimo!";
     } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "../assets/img/menuIcon.svg";
+      textResult = "Você escolheu NÃO. Talvez na próxima vez!";
     }
+    document.querySelector("#result").innerText = textResult;
+  }
 }
