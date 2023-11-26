df42b3beac8742463ac5cdc7856f3130644b6290
bitoller <bi_toller@hotmail.com>
2023-11-26 19:45:17 -0300

    remove survey

--- src/scripts/treatment.js
+++ src/scripts/treatment.js
@@ -9,26 +9,3 @@ function menuShow() {
     document.querySelector(".icon").src = "../assets/img/menuIcon.svg";
   }
 }
-
-function displayRadioResult() {
-  let selectedOption = document.querySelector(
-    "input[name='mortality']:checked"
-  );
-
-  if (selectedOption) {
-    let textResult = "";
-
-    if (selectedOption.value === "yes") {
-      textResult = "Você escolheu SIM. Isso é ótimo!";
-    } else {
-      textResult = "Você escolheu NÃO. Talvez na próxima vez!";
-    }
-    document.querySelector("#result").innerText = textResult;
-  }
-}
-
-function modalInputReset() {
-  document.getElementById("myForm").reset();
-  document.getElementById("result").innerText =
-    "Preencha o questionário para ver seu resultado.";
-}
