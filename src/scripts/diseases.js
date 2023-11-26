589678a0931715c4ef82b8623f2f292630d69365
bitoller <bi_toller@hotmail.com>
2023-11-26 19:45:07 -0300

    add survey content

--- src/scripts/diseases.js
+++ src/scripts/diseases.js
@@ -9,3 +9,73 @@ function menuShow() {
     document.querySelector(".icon").src = "../assets/img/menuIcon.svg";
   }
 }
+
+function displayRadioResult() {
+  let vaccinesOption = document.querySelector("input[name='vaccines']:checked");
+  let handWashOption = document.querySelector(
+    "input[name='hand-wash']:checked"
+  );
+  let maskOption = document.querySelector("input[name='mask']:checked");
+
+  if (vaccinesOption && handWashOption && maskOption) {
+    let textResult = "";
+
+    if (
+      vaccinesOption.value == "good" &&
+      handWashOption.value == "good" &&
+      maskOption.value == "good"
+    ) {
+      textResult =
+        "Parabéns pelo seu comprometimento em usar máscara, lavar as mãos e tomar vacina. Essas ações demonstram responsabilidade e cuidado com a saúde, contribuindo para a segurança coletiva.";
+    } else if (
+      (vaccinesOption.value == "good" || vaccinesOption.value == "okay") &&
+      (handWashOption.value == "good" || handWashOption.value == "okay") &&
+      maskOption.value == "bad"
+    ) {
+      textResult =
+        "Você já tem boas práticas. Incentivamos o uso da máscara para proteger a saúde de todos. Considere essa prática essencial.";
+    } else if (
+      (maskOption.value == "good" || maskOption.value == "okay") &&
+      (handWashOption.value == "good" || handWashOption.value == "okay") &&
+      vaccinesOption.value == "bad"
+    ) {
+      textResult =
+        "Você já tem boas práticas. Destacamos a importância da vacinação para proteger sua saúde e a comunidade. Considere tomar a vacina como um passo fundamental para a segurança coletiva.";
+    } else if (
+      (vaccinesOption.value == "good" || vaccinesOption.value == "okay") &&
+      (maskOption.value == "good" || maskOption.value == "okay") &&
+      handWashOption.value == "bad"
+    ) {
+      textResult =
+        "Você já tem boas práticas. Destacamos a relevância de lavar as mãos para a saúde pessoal e coletiva. Incorporar esse hábito é crucial para prevenir doenças. Considere torná-lo parte de sua rotina diária.";
+    } else if (
+      (vaccinesOption.value == "good" || vaccinesOption.value == "okay") &&
+      (handWashOption.value == "good" || handWashOption.value == "okay") &&
+      (maskOption.value == "good" || maskOption.value == "okay")
+    ) {
+      textResult =
+        "Seu comprometimento em manter práticas de prevenção da saúde está em um nível mediano para ótimo. Apresentamos abaixo na página alguns tópicos para melhorar seus hábitos e buscar uma vida mais saudável.";
+    } else if (
+      vaccinesOption.value == "bad" &&
+      handWashOption.value == "bad" &&
+      maskOption.value == "bad"
+    ) {
+      textResult =
+        "Incentivamos a adoção de práticas como usar máscara, lavar as mãos e tomar vacina para preservar a saúde. Esses cuidados contribuem para a segurança coletiva. Considere incorporar essas medidas em sua rotina.";
+    } else {
+      textResult =
+        "Precisa melhorar a atenção às práticas de saúde. Sugerimos mais dedicação, pois é crucial para o bem-estar pessoal e coletivo.";
+    }
+    document.querySelector("#result").innerText = textResult;
+  } else {
+    textResult =
+      "Você deixou alguma resposta em branco. Por favor responda todas as perguntas para ver seu resultado.";
+    document.querySelector("#result").innerText = textResult;
+  }
+}
+
+function modalInputReset() {
+  document.getElementById("myForm").reset();
+  document.getElementById("result").innerText =
+    "Preencha o questionário para ver seu resultado.";
+}
--- src/scripts/improvement.js
+++ src/scripts/improvement.js
@@ -11,18 +11,126 @@ function menuShow() {
 }
 
 function displayRadioResult() {
-  let selectedOption = document.querySelector(
-    "input[name='mortality']:checked"
+  let physicalActivityOption = document.querySelector(
+    "input[name='physical-activity']:checked"
+  );
+  let foodHabitOption = document.querySelector(
+    "input[name='food-habit']:checked"
+  );
+  let sleepHabitOption = document.querySelector(
+    "input[name='sleep-habit']:checked"
+  );
+  let riskBehaviorOption = document.querySelector(
+    "input[name='risk-behavior']:checked"
+  );
+  let healthCheckOption = document.querySelector(
+    "input[name='health-check']:checked"
   );
 
-  if (selectedOption) {
+  if (
+    physicalActivityOption &&
+    foodHabitOption &&
+    sleepHabitOption &&
+    riskBehaviorOption &&
+    healthCheckOption
+  ) {
     let textResult = "";
 
-    if (selectedOption.value === "yes") {
-      textResult = "Você escolheu SIM. Isso é ótimo!";
+    if (
+      physicalActivityOption.value == "good" &&
+      foodHabitOption.value == "good" &&
+      sleepHabitOption.value == "good" &&
+      riskBehaviorOption.value == "good" &&
+      healthCheckOption.value == "good"
+    ) {
+      textResult =
+        "Você leva uma vida notavelmente saudável. Parabéns pelo cuidado com a saúde!";
+    } else if (
+      (foodHabitOption.value == "good" || foodHabitOption.value == "okay") &&
+      (sleepHabitOption.value == "good" || sleepHabitOption.value == "okay") &&
+      (riskBehaviorOption.value == "good" ||
+        riskBehaviorOption.value == "okay") &&
+      (healthCheckOption.value == "good" ||
+        healthCheckOption.value == "okay") &&
+      physicalActivityOption.value == "bad"
+    ) {
+      textResult =
+        "A falta de atividade física pode levar a uma série de problemas de saúde, incluindo ganho de peso, redução da energia e maior suscetibilidade a condições médicas adversas.";
+    } else if (
+      (physicalActivityOption.value == "good" ||
+        physicalActivityOption.value == "okay") &&
+      (sleepHabitOption.value == "good" || sleepHabitOption.value == "okay") &&
+      (riskBehaviorOption.value == "good" ||
+        riskBehaviorOption.value == "okay") &&
+      (healthCheckOption.value == "good" ||
+        healthCheckOption.value == "okay") &&
+      foodHabitOption.value == "bad"
+    ) {
+      textResult =
+        "Uma alimentação inadequada pode resultar em diversos problemas de saúde, incluindo falta de energia, ganho de peso excessivo e impactos negativos no funcionamento geral do corpo.";
+    } else if (
+      (foodHabitOption.value == "good" || foodHabitOption.value == "okay") &&
+      (physicalActivityOption.value == "good" ||
+        physicalActivityOption.value == "okay") &&
+      (riskBehaviorOption.value == "good" ||
+        riskBehaviorOption.value == "okay") &&
+      (healthCheckOption.value == "good" ||
+        healthCheckOption.value == "okay") &&
+      sleepHabitOption.value == "bad"
+    ) {
+      textResult =
+        "A falta de descanso prejudica a saúde, impactando negativamente o sistema imunológico, aumentando o estresse e comprometendo o funcionamento cognitivo.";
+    } else if (
+      (foodHabitOption.value == "good" || foodHabitOption.value == "okay") &&
+      (sleepHabitOption.value == "good" || sleepHabitOption.value == "okay") &&
+      (physicalActivityOption.value == "good" ||
+        physicalActivityOption.value == "okay") &&
+      (healthCheckOption.value == "good" ||
+        healthCheckOption.value == "okay") &&
+      riskBehaviorOption.value == "bad"
+    ) {
+      textResult =
+        "O consumo de tabaco, bebidas alcoólicas e drogas prejudica a saúde, aumentando o risco de doenças, comprometendo o bem-estar mental e físico.";
+    } else if (
+      (foodHabitOption.value == "good" || foodHabitOption.value == "okay") &&
+      (sleepHabitOption.value == "good" || sleepHabitOption.value == "okay") &&
+      (riskBehaviorOption.value == "good" ||
+        riskBehaviorOption.value == "okay") &&
+      (physicalActivityOption.value == "good" ||
+        physicalActivityOption.value == "okay") &&
+      healthCheckOption.value == "bad"
+    ) {
+      textResult =
+        "Procure ir mais ao médico para realização de exames e check-ups de rotina, a maioria das doenças descobertas de forma precoce são remediáveis!";
+    } else if (
+      (foodHabitOption.value == "good" || foodHabitOption.value == "okay") &&
+      (sleepHabitOption.value == "good" || sleepHabitOption.value == "okay") &&
+      (riskBehaviorOption.value == "good" ||
+        riskBehaviorOption.value == "okay") &&
+      (healthCheckOption.value == "good" ||
+        healthCheckOption.value == "okay") &&
+      (physicalActivityOption.value == "good" ||
+        physicalActivityOption.value == "okay")
+    ) {
+      textResult =
+        "Você leva uma vida razoavelmente saudável. Logo abaixo na página, temos alguns tópicos sobre hábitos para uma vida mais saudável.";
+    } else if (
+      foodHabitOption.value == "bad" &&
+      sleepHabitOption.value == "bad" &&
+      riskBehaviorOption.value == "bad" &&
+      healthCheckOption.value == "bad" &&
+      physicalActivityOption.value == "bad"
+    ) {
+      textResult =
+        "Diante de uma saúde bastante comprometida, apresentamos abaixo na página alguns tópicos para melhorar seus hábitos e buscar uma vida mais saudável.";
     } else {
-      textResult = "Você escolheu NÃO. Talvez na próxima vez!";
+      textResult =
+        "Você está deixando passar alguns hábitos saudáveis, é possível aprimorar ainda mais sua abordagem para uma vida mais saudável. Confira abaixo algumas sugestões para otimizar seus cuidados e não perder nenhum hábito benéfico.";
     }
+    document.querySelector("#result").innerText = textResult;
+  } else {
+    textResult =
+      "Você deixou alguma resposta em branco. Por favor responda todas as perguntas para ver seu resultado.";
     document.querySelector("#result").innerText = textResult;
   }
 }
